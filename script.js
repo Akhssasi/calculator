9039937f50d9480c039259827d1163ff19f9bbba
mohamed <akhssassi18@gmail.com>
2025-04-10 23:45:39 +0400

    copy code js in file html to js file

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("calculator")
+function appendNumber(number) {
+    document.getElementById('result').value += number;
+}
+
+function appendOperator(operator) {
+    document.getElementById('result').value += operator;
+}
+
+function calculate() {
+    try {
+        var result = eval(document.getElementById('result').value);
+        document.getElementById('result').value = result;
+    } catch (error) {
+        document.getElementById('result').value = 'Error';
+    }
+}
+
+function deleteCharacter() {
+    var result = document.getElementById('result').value;
+    document.getElementById('result').value = result.slice(0, -1);
+}
+
+function clearResult() {
+    document.getElementById('result').value = '';
+}
